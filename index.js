5262e1685787832df1f5e48c7a29faa3d2667061
UPLCC <143597013+UPLCC@users.noreply.github.com>
2025-07-30 17:39:41 +0800

    Update index.js
    .

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+function help(){
+helpfh=console.log("提示(value):无返回\n打印(value):无返回\nloc存储(key,value):无返回\nloc读取(key):返回变量locdq\nloc删除(key):无返回\nloc删除全部():无返回\nloc遍历():返回变量locfh,locfha,locfhb\n写入(value):无返回\n输入弹窗(value):返回变量srtcfh\n跳转(value):无返回");
+}
 function 提示(value){
 alert(value);
 }
@@ -27,10 +30,10 @@ var locval=localStorage.getItem(lockey);
 locfh+=lockey+","+locval;
 locfha+=lockey+","+locval+"\n";
 locfhb+=lockey+","+locval+"<br>";
+}
 return locfh;
 return locfha;
 return locfhb;
-}
 }
 function 写入(value){
 document.write(value);
